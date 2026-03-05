879768d1779a2bcf3f01cb3ef26e1d697bc500a3
toanysd <toan.ysd@gmail.com>
2026-03-05 12:25:46 +0900

    Add files via upload

--- moldcutter-backend/server-v8.2.7.js
+++ moldcutter-backend/server-v8.2.7.js
@@ -29,8 +29,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
-
+app.options(/.*/, cors(corsOptions));
 app.use(bodyParser.json({ limit: '4mb' }));
 
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
